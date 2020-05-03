7802a5d4f47e023239a554363e37091d36ccb7e6
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-05-03 15:37:49 +0300

    Added react-native-router-flux with react-navigation v4

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Scene, Router, Drawer, Overlay} from 'react-native-router-flux';
 
 import Charts from './src/components/Charts';
 
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Overlay key="overlay">
+        <Scene key="root">
+          <Drawer drawerPosition="left" drawerWidth={300}>
+            <Scene key="main" title="KubeControl" component={Charts} />
+          </Drawer>
+        </Scene>
+      </Overlay>
+    </Router>
+  );
+};
+
 class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Charts />
-      </View>
-    );
+    return <RouterComponent />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
